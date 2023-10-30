d806270d5d2ec4eb20750462f0f35ec594b63988
AlexMikhalev <alex@metacortex.engineer>
2023-10-30 21:16:44 +0000

    New persistance layer with global config and write everywhere
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- crates/persistance/src/lib.rs
+++ crates/persistance/src/lib.rs
@@ -44,29 +44,25 @@ pub trait Persistable: Serialize + serde::de::DeserializeOwned {
     async fn save(&self)->Result<()>;
     async fn load(&mut self, key:&str)->Result<Self> where Self: Sized;
     async fn load_config(&self)->Result<(Vec<(Operator,u128)>,Operator)> {
+        // TODO: add for each operator save
+        // TODO add load from fastest operator
         let state = State::instance().await;
         Ok((state.ops.clone(), state.fastest_op.clone()))
-        // let d = config_dir().ok_or_else(|| anyhow!("unknown config dir")).unwrap();
-        // let default_config_path = d.join("terraphim/config.toml");
-        // let cfg = config::Config::load(default_config_path.as_path()).unwrap();
-        // println!("cfg: {:?}", cfg);
-        // let ops = cfg.parse_profiles().await?;
-        // let fastest_op = ops
-        //     .iter()
-        //     .min_by_key(|op| op.1)
-        //     .ok_or_else(|| anyhow!("No operators provided")).unwrap().0.clone();
-    
-        // Ok((ops,fastest_op))
+
     }
     async fn save_to_operator(&self, op: &Operator) -> Result<()> {
+        let (ops, fastest_op) = &self.load_config().await?;
         let key = self.get_key();
         let serde_str=serde_json::to_string(&self).unwrap();
-        op.write(key.as_str(), serde_str).await?;
+        for (op, _time) in ops {
+            op.write(&key, serde_str.clone()).await?;
+        }
         Ok(())
     }
     
-    async fn load_from_operator(key:&str, op: &Operator) -> Result<Self> where Self: Sized {
-        let bs = op.read(key).await?;
+    async fn load_from_operator(&self, key:&str, op: &Operator) -> Result<Self> where Self: Sized {
+        let (ops, fastest_op) = &self.load_config().await?;
+        let bs = fastest_op.read(key).await?;
         let obj = serde_json::from_slice(&bs).unwrap();
         Ok(obj)
     }
@@ -74,34 +70,3 @@ pub trait Persistable: Serialize + serde::de::DeserializeOwned {
     fn get_key(&self) -> String;
 }
 
-// pub fn init_operator_via_map()->Result<Operator>{
-
-//             let builder = services::Dashmap::default();
-//             // Init an operator
-//             let op = Operator::new(builder)?
-//                 // Init with logging layer enabled.
-//                 .layer(LoggingLayer::default())
-//                 .finish();
-//             debug!("operator: {op:?}");
-//             Ok(op)
-// }
-
-
-
-pub fn init_operator_via_map() -> Result<Operator> {
-    // setting up the credentials
-    let access_key_id =
-        env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID is set and a valid String");
-    let secret_access_key =
-        env::var("AWS_SECRET_ACCESS_KEY").expect("AWS_ACCESS_KEY_ID is set and a valid String");
-
-    let mut map = HashMap::default();
-    map.insert("bucket".to_string(), "test".to_string());
-    map.insert("region".to_string(), "us-east-1".to_string());
-    map.insert("endpoint".to_string(), "http://rpi4node3:8333".to_string());
-    map.insert("access_key_id".to_string(), access_key_id);
-    map.insert("secret_access_key".to_string(), secret_access_key);
-
-    let op = Operator::via_map(Scheme::S3, map)?;
-    Ok(op)
-}
