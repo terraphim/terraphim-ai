2cd1a342e8b1a5569d21ab3b0984a9b39ec4f76d
AlexMikhalev <alex@metacortex.engineer>
2023-11-17 13:45:10 +0000

    pair programming results before clippy
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- crates/terraphim_pipeline/src/input.rs
+++ crates/terraphim_pipeline/src/input.rs
@@ -1,6 +1,6 @@
-pub const TEST1: &'static str = include_str!("../data/system_operator_cc/test1.md");
-pub const TEST12: &'static str = include_str!("../data/system_operator_cc/test12.md");
-pub const TEST123: &'static str = include_str!("../data/system_operator_cc/test123.md");
-pub const TEST1234: &'static str = include_str!("../data/system_operator_cc/test1234.md");
-pub const TEST12345: &'static str = include_str!("../data/system_operator_cc/test12345.md");
+pub const TEST1: &str = include_str!("../data/system_operator_cc/test1.md");
+pub const TEST12: &str = include_str!("../data/system_operator_cc/test12.md");
+pub const TEST123: &str = include_str!("../data/system_operator_cc/test123.md");
+pub const TEST1234: &str = include_str!("../data/system_operator_cc/test1234.md");
+pub const TEST12345: &str = include_str!("../data/system_operator_cc/test12345.md");
 pub const TEST_CORPUS: &[&str] = &[TEST1, TEST12, TEST123, TEST1234, TEST12345];
