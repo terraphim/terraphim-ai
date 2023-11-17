815d87f89bc7d47c719273db17e9faacd225816a
AlexMikhalev <alex@metacortex.engineer>
2023-11-17 12:11:41 +0000

    pair programming results
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

---
+++ server/README.md
@@ -0,0 +1,9 @@
+TODO 
+=====
+
+[x] Wrap relegraph.parse_document_to_pair into poem open api 
+[x] Add poem api for query
+[] Fix two bugs in query API
+[] Check rolegraph.query function (it can be optimised using BinaryHeap or BtreeMap)BinaryHeap
+[] Go through RoleGraph struct to see if any obvious mistakes
+[] Add persistence to document struct - fun part.
---
+++ server/src/types.rs
@@ -0,0 +1,53 @@
+use poem_openapi::{Object, Tags};
+use serde::{Deserialize, Serialize};
+use terraphim_pipeline::{Document, IndexedDocument};
+use poem_openapi::types::{ToJSON};
+
+#[derive(Debug, Object)]
+pub struct SearchQuery {
+    pub(crate) search_term: String,
+    skip: usize,
+    limit: usize,
+    role: Option<String>,
+}
+
+/// Create article schema
+#[derive(Deserialize, Serialize, Debug, Object)]
+pub(crate) struct Article {
+    pub(crate) id: Option<String>,
+    pub(crate) stub: Option<String>,
+    pub(crate) title: String,
+    pub(crate) url: String,
+    pub(crate) body: String,
+    pub(crate) description: Option<String>,
+    pub(crate) tags: Option<Vec<String>>,
+}
+
+impl Into<Document> for Article {
+    fn into(self) -> Document {
+        // If the ID is not provided, generate a new one
+        let id = match self.id {
+            Some(id) => id,
+            None => ulid::Ulid::new().to_string(),
+        };
+
+        Document {
+            id,
+            title: self.title,
+            body: Some(self.body),
+            description: self.description,
+        }
+    }
+}
+
+
+#[derive(Tags)]
+pub(crate) enum ApiTags {
+    /// Article operations
+    Article,
+    /// Config operations
+    Config,
+    /// Search operations
+    Search,
+    Save,
+}
