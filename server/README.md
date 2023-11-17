9fbc2ad2994279aab8db1c7e75c5c02bd36970ef
AlexMikhalev <alex@metacortex.engineer>
2023-11-17 13:20:24 +0000

    pair programming results before fmt
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- server/README.md
+++ server/README.md
@@ -3,7 +3,9 @@ TODO
 
 [x] Wrap relegraph.parse_document_to_pair into poem open api 
 [x] Add poem api for query
-[] Fix two bugs in query API
+[x] Convert unwrap to errors
+[ ] Error handling for api
+[ ] Fix JSON serialization bug
 [] Check rolegraph.query function (it can be optimised using BinaryHeap or BtreeMap)BinaryHeap
 [] Go through RoleGraph struct to see if any obvious mistakes
 [] Add persistence to document struct - fun part.
--- server/src/main.rs
+++ server/src/main.rs
@@ -14,19 +14,20 @@
 #![deny(anonymous_parameters, macro_use_extern_crate, pointer_structural_match)]
 #![deny(missing_docs)]
 
-use poem::{listener::TcpListener, EndpointExt, Result, Route, Server};
+use poem::{listener::TcpListener, EndpointExt, Route, Server};
 use poem_openapi::OpenApiService;
 mod settings;
 use settings::Settings;
 use terraphim_pipeline::{Document, RoleGraph};
 use tokio::sync::Mutex;
+use anyhow::{Context, Result};
 
 mod api;
 use api::Api;
 mod types;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let settings = Settings::new().unwrap();
     println!("{:?}", settings);
@@ -36,7 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let role = "system operator".to_string();
     // let automata_url = "https://system-operator.s3.eu-west-2.amazonaws.com/term_to_id.json";
     let automata_url = "./data/term_to_id.json";
-    let rolegraph = RoleGraph::new(role, automata_url);
+    let rolegraph = RoleGraph::new(role, automata_url).context(format!("Failed to create rolegraph from {automata_url}"))?;
 
     let api_service = OpenApiService::new(Api { rolegraph: Mutex::new(rolegraph) }, "Hello World", "1.0").server(api_endpoint);
     let ui = api_service.swagger_ui();
