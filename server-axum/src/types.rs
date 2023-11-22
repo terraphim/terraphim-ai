fa618d49b8805a5fab6bca5d14ad67f5883382e0
AlexMikhalev <alex@metacortex.engineer>
2023-11-22 00:41:18 +0000

    First working version of Axum web api with create and search API. WIP
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

---
+++ server-axum/src/types.rs
@@ -0,0 +1,44 @@
+use terraphim_pipeline::{Document, IndexedDocument};
+use ulid::Ulid;
+use serde::{Deserialize, Serialize};
+use utoipa::{ToSchema, IntoParams};
+
+
+
+
+#[derive(Debug, Deserialize, IntoParams)]
+pub struct SearchQuery {
+    pub(crate) search_term: String,
+    pub(crate) skip: usize,
+    pub(crate) limit: usize,
+    pub (crate) role: Option<String>,
+}
+
+/// Create article schema
+#[derive(Deserialize, Serialize, Debug, ToSchema, Clone)]
+pub(crate) struct Article {
+    pub(crate) id: Option<String>,
+    pub(crate) stub: Option<String>,
+    pub(crate) title: String,
+    pub(crate) url: String,
+    pub(crate) body: String,
+    pub(crate) description: Option<String>,
+    pub(crate) tags: Option<Vec<String>>,
+}
+
+impl Into<Document> for Article {
+    fn into(self) -> Document {
+        // If the ID is not provided, generate a new one
+        let id = match self.id {
+            Some(id) => id,
+            None => ulid::Ulid::new().to_string(),
+        };
+
+        Document {
+            id,
+            title: self.title,
+            body: Some(self.body),
+            description: self.description,
+        }
+    }
+}
