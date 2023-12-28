cc8c145a0c709fee5cec19fbf79f5e96ba4c54be
AlexMikhalev <alex@metacortex.engineer>
2023-12-28 17:57:21 +0000

    Streaming results via SSE handler - untested
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- server-axum/src/main.rs
+++ server-axum/src/main.rs
@@ -14,13 +14,13 @@
 #![deny(anonymous_parameters, macro_use_extern_crate, pointer_structural_match)]
 // #![deny(missing_docs)]
 
-use std::{error::Error, net::SocketAddr, collections::HashMap};
+use std::{error::Error, net::SocketAddr};
 use axum::{
     extract::{Query, State},
     http::StatusCode,
-    response::IntoResponse,
+    response::{sse::Event, IntoResponse, Sse, Response},
     routing::{get, post},
-    Json, Router
+    Json, Router, Extension
 };
 use ulid::Ulid;
 use std::sync::Arc;
@@ -30,202 +30,70 @@ use terraphim_config::TerraphimConfig;
 use terraphim_settings::Settings;
 
 mod types;
+mod api;
+
+use api::{create_article,search_articles,show_config,health_axum, search_articles_stream};
+
+use tokio::sync::broadcast::channel;
+
+
+
 
 /// TODO: Can't get Open API docs to work with axum consistencly,given up for now.
 
-use utoipa::{
-   OpenApi,
-};
-
-
-
-
-
-
 use terraphim_pipeline::{RoleGraph, IndexedDocument};
 
-#[derive(Debug, Clone)]
-pub(crate) struct ConfigState {
-    /// Terraphim Config
-    pub(crate) config: Arc<Mutex<TerraphimConfig>>,
-    pub(crate) roles: HashMap<String, RoleGraphState>
+use portpicker;
+
+pub async fn app(server_hostname:SocketAddr, config_state:types::ConfigState) {
+    let (tx, _rx) = channel::<IndexedDocument>(10);
+    let app = Router::new()
+    .route("/", get(health_axum))
+    .route("/health", get(health_axum))
+    // .route("/articles", get(list_articles))
+    .route("/article", post(create_article))
+    .route("/articles/search", get(search_articles))
+    .route("/config/", get(api::show_config))
+    .route("/articles/search/stream", get(search_articles_stream))
+    .with_state(config_state)
+    .layer(Extension(tx));
+     println!("listening on {server_hostname}");
+     axum::Server::bind(&server_hostname)
+     .serve(app.into_make_service())
+     .await
+     .unwrap_or_else(|_| {
+        // FIXME: this unwrap is never reached
+        // the desired behavior is to pick a new port and try again
+        let port = portpicker::pick_unused_port().expect("failed to find unused port");
+        let add=SocketAddr::from(([127, 0, 0, 1], port));
+        println!("listening on {add} with {port}");
+    });
+     
+    
 }
-
-#[derive(Debug, Clone)]
-pub(crate) struct RoleGraphState {
-    /// RoleGraph for ingesting documents
-    pub(crate) rolegraph: Arc<Mutex<RoleGraph>>,
-}
-
-#[derive(OpenApi, Debug)]
-#[openapi(paths(health_axum, create_article, search_articles), components(schemas(types::Article)))]
-pub struct ApiDoc;
-
-#[utoipa::path(
-    get,
-    path = "/health",
-    responses(
-        (status = 200, description = "Health Check")
-    )
-)]
-pub async fn health_axum() -> impl IntoResponse {
-    (StatusCode::OK, "OK")
-}
-
-#[utoipa::path(
-    post,
-    path = "/article",
-    request_body = Article,
-    responses(
-        (status = 201, description = "Article item created successfully", body = types::Article),
-        (status = 409, description = "Create failed")
-    )
-)]
-/// Creates index of the article for each rolegraph
-async fn create_article(State(config): State<ConfigState>,Json(article): Json<types::Article>) -> impl IntoResponse {
-    log::warn!("create_article");
-    let mut article = article.clone();
-    let id = Ulid::new().to_string();
-    let id = if article.id.is_none() {
-        article.id = Some(id.clone());
-        id
-    } else {
-        article.id.clone().unwrap()
-    };
-    for rolegraph_state in config.roles.values() {
-        let mut rolegraph = rolegraph_state.rolegraph.lock().await;
-        rolegraph.parse_document(id.clone(), article.clone());
-    }
-    log::warn!("send response");
-    let response= Json(article.clone());
-    (StatusCode::CREATED, response)
-}
-
-#[utoipa::path(
-    get,
-    path = "/article",
-    responses(
-        (status = 200, description = "List all articles successfully", body = [types::Article]),
-    )
-)]
-async fn list_articles(State(rolegraph): State<Arc<Mutex<RoleGraph>>>) -> impl IntoResponse {
-
-    let rolegraph = rolegraph.lock().await.clone();
-    println!("{rolegraph:?}");
-
-    (StatusCode::OK, Json("Ok"))
-}
-
-    /// Search All TerraphimGraphs defined in a config by query params.
-    #[utoipa::path(
-        get,
-        path = "/articles/search",
-        params(
-            types::SearchQuery
-        ),
-        responses(
-            (status = 200, description = "List matching articles by query", body = [types::Article]),
-        )
-    )]
-async fn search_articles(State(config): State<ConfigState>,search_query: Query<types::SearchQuery>) -> Json<Vec<IndexedDocument>>{
-    println!("Searching articles with query: {search_query:?}");
-    let default_role = config.config.lock().await.default_role.clone();
-    // if role is not provided, use the default role in the config
-    let role = if search_query.role.is_none() {
-        default_role.as_str()
-    } else {
-        search_query.role.as_ref().unwrap()
-    };
-    // let role = search_query.role.as_ref().unwrap();
-    let rolegraph = config.roles.get(role).unwrap().rolegraph.lock().await;
-    let documents: Vec<(&String, IndexedDocument)> =
-    match rolegraph.query(&search_query.search_term, search_query.skip, search_query.limit) {
-        Ok(docs) => docs,
-        Err(e) => {
-            log::error!("Error: {}", e);
-            return Json(vec![]);
-        }
-    };
-    let docs: Vec<IndexedDocument> = documents.into_iter().map(|(_id, doc) | doc).collect();
-    println!("Found articles: {docs:?}");
-    Json(docs)
-}
-
-/// API handler for Terraphim Config
-async fn show_config(State(config):State<ConfigState>)-> Json<TerraphimConfig> {
-    let config=config.config.lock().await;
-    Json(config.clone())
-}
-
-/// Search articles by query params, subscribe to results via websocket.
-/// 
-// async fn ws_handle(
-//     Query(search_query): Query<types::SearchQuery>,
-//     ws: WebSocketUpgrade,
-// ) -> Response {
-//     // do something with `params`
-
-//     ws.on_upgrade(handle_socket)
-// }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let server_settings = Settings::load_from_env_and_file(None);
     println!("Device settings hostname: {:?}", server_settings.server_hostname);
     let server_hostname = server_settings.server_hostname.unwrap().parse::<SocketAddr>().unwrap_or_else(|_| {
-        SocketAddr::from(([127, 0, 0, 1], 8000))
+        let port = portpicker::pick_unused_port().expect("failed to find unused port");
+        SocketAddr::from(([127, 0, 0, 1], port))
     });
-    let config=TerraphimConfig::new();
-    let mut config_state= ConfigState {
-        config: Arc::new(Mutex::new(config.clone())),
-        roles: HashMap::new()
-    };
+    let mut config_state= types::ConfigState::new().await?;
 
-    // for each role in a config initialize a rolegraph
-    // and add it to the config state
-    for (role_name,each_role) in config.roles {
-        let automata_url= each_role.kg.automata_url.as_str();
-        println!("{} - {}",role_name.clone(),automata_url);
-        let rolegraph = RoleGraph::new(role_name.clone(), automata_url).await?;        
-        config_state.roles.insert(role_name, RoleGraphState {
-            rolegraph: Arc::new(Mutex::new(rolegraph))
-        });
-
-    }
     // Add one more for testing local KG
-    
 
-    
     let addr = server_hostname;
     let role = "system operator".to_string();
     // let automata_url = "https://system-operator.s3.eu-west-2.amazonaws.com/term_to_id.json";
     let automata_url = "./data/term_to_id.json";
     let rolegraph = RoleGraph::new(role.clone(), automata_url).await?;        
-    config_state.roles.insert(role, RoleGraphState {
+    config_state.roles.insert(role, types::RoleGraphState {
         rolegraph: Arc::new(Mutex::new(rolegraph))
     });
     println!("cfg Roles: {:?}", config_state.roles.keys().collect::<Vec<&String>>());
-    let app = Router::new()
-        .route("/", get(health_axum))
-        // .route("/articles", get(list_articles))
-        .route("/article", post(create_article))
-        .route("/articles/search", get(search_articles))
-        .route("/config", get(show_config))
-        // .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
-        // There is no need to create `RapiDoc::with_openapi` because the OpenApi is served
-        // via SwaggerUi instead we only make rapidoc to point to the existing doc.
-        // .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
-        // .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()))
-        // .with_state(rolegraph)
-        .with_state(config_state);
-
-    println!("listening on {addr}");
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    app(addr, config_state).await;
 
     Ok(())
 }
@@ -234,7 +102,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 mod tests {
     use tokio::test;
     use reqwest::{StatusCode, Client};
-    use tokio::runtime::Runtime;
+    
     #[test]
     async fn test_search_articles() {
         
@@ -268,7 +136,7 @@ mod tests {
     #[test]
     async fn test_get_config() {
         
-            let response = reqwest::get("http://localhost:8000/config").await.unwrap();
+            let response = reqwest::get("http://localhost:8000/config/").await.unwrap();
             assert_eq!(response.status(), StatusCode::OK);
             // You can also test the response body if you want:
             // let body = response.text().await.unwrap();
--- server-axum/src/types.rs
+++ server-axum/src/types.rs
@@ -1,11 +1,13 @@
 use terraphim_pipeline::{Document};
 
 use serde::{Deserialize, Serialize};
-use utoipa::{ToSchema, IntoParams};
+
+
+
 
 /// Query type for searching documents in the `RoleGraph`.
 /// It contains the search term, skip and limit parameters.
-#[derive(Debug, Serialize, Deserialize, IntoParams, ToSchema)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub(crate) struct SearchQuery {
     pub(crate) search_term: String,
     pub(crate) skip: Option<usize>,
@@ -14,7 +16,7 @@ pub(crate) struct SearchQuery {
 }
 
 /// Create article schema
-#[derive(Deserialize, Serialize, Debug, ToSchema, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub(crate) struct Article {
     pub(crate) id: Option<String>,
     pub(crate) stub: Option<String>,
@@ -40,4 +42,50 @@ impl From<Article> for Document {
             description: val.description,
         }
     }
+}
+
+use terraphim_pipeline::{RoleGraph, IndexedDocument};
+use tokio::sync::Mutex;
+use anyhow::{Result};
+use terraphim_config::TerraphimConfig;
+
+use std::sync::Arc;
+use std::collections::HashMap;
+
+
+
+#[derive(Debug, Clone)]
+pub(crate) struct ConfigState {
+    /// Terraphim Config
+    pub(crate) config: Arc<Mutex<TerraphimConfig>>,
+    pub(crate) roles: HashMap<String, RoleGraphState>
+}
+
+impl ConfigState {
+    pub(crate) async fn new() -> Result<Self> {
+        let config=TerraphimConfig::new();
+        let mut config_state= ConfigState {
+            config: Arc::new(Mutex::new(config.clone())),
+            roles: HashMap::new()
+        };
+    
+        // for each role in a config initialize a rolegraph
+        // and add it to the config state
+        for (role_name,each_role) in config.roles {
+            let automata_url= each_role.kg.automata_url.as_str();
+            println!("{} - {}",role_name.clone(),automata_url);
+            let rolegraph = RoleGraph::new(role_name.clone(), automata_url).await?;        
+            config_state.roles.insert(role_name, RoleGraphState {
+                rolegraph: Arc::new(Mutex::new(rolegraph))
+            });
+    
+        }
+        Ok(config_state)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct RoleGraphState {
+    /// RoleGraph for ingesting documents
+    pub(crate) rolegraph: Arc<Mutex<RoleGraph>>,
 }
