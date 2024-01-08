3d3c6b57ef747ba5d8a9343e6c2af2b2cb523184
Matthias <matthias-endler@gmx.net>
2024-01-08 13:33:12 +0100

    Introduce `Middleware` trait
    This introduces the concept of a `Middleware` trait, which allows us to
    find a `needle` in a `haystack` across different services.
    
    While I was at it, I refactored the code a bit:
    
    - I ended up removing the `unwrap_` methods, as the `match` block already
      gives us the correct message type, so we can avoid the `unwrap` altogether.
    - Removed unused imports.
    - Cleaned up some old `println!` statements.

--- crates/middleware/src/ripgrep_service.rs
+++ crates/middleware/src/ripgrep_service.rs
@@ -1,7 +1,4 @@
-
-use terraphim_types::{ConfigState, SearchQuery, Article, merge_and_serialize};
-use terraphim_pipeline::IndexedDocument; 
-use std::collections::HashMap;
+use terraphim_pipeline::IndexedDocument;
+use terraphim_types::{merge_and_serialize, Article, ConfigState, SearchQuery};
 
 use terraphim_middleware::run_ripgrep_service_and_index;
-
