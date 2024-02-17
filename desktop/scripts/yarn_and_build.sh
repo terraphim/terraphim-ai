8545d1b89b128ac08453dc18482a0bea59956865
AlexMikhalev <alex@metacortex.engineer>
2024-02-17 12:27:26 +0000

    fixed path
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- desktop/scripts/yarn_and_build.sh
+++ desktop/scripts/yarn_and_build.sh
@@ -4,4 +4,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 bash -c "source $HOME/.nvm/nvm.sh && nvm install 16.15.1"
 bash -c "source $HOME/.nvm/nvm.sh && npm install -g yarn"
 bash -c "source $HOME/.nvm/nvm.sh && cd /code/desktop && yarn && yarn build"
-cp -Rv /code/desktop/dist /code/terraphim-server/
+cp -Rv /code/desktop/dist /code/terraphim_server/
