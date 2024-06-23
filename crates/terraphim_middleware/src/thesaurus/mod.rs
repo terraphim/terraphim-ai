66b2d04a25cb985e4e07f5e1d732cc46f8fb1ef2
AlexMikhalev <alex@metacortex.engineer>
2024-06-23 19:21:30 +0100

    filtering ripgrep by markdown type
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- crates/terraphim_middleware/src/thesaurus/mod.rs
+++ crates/terraphim_middleware/src/thesaurus/mod.rs
@@ -158,7 +158,7 @@ impl Default for LogseqService {
     fn default() -> Self {
         Self {
             command: "rg".to_string(),
-            default_args: ["--json", "--trim", "--ignore-case"]
+            default_args: ["--json", "--trim", "--ignore-case","-tmarkdown"]
                 .into_iter()
                 .map(String::from)
                 .collect(),
